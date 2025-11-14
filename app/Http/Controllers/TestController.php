c4e0d748d2ab4d612efc38e59724e421170538c4
jeffhuang <jeffhuang@besttour.com.tw>
2025-11-14 09:11:47 +0800

    移除不必要的方法 idontwantthismethod

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -11,4 +11,9 @@ class TestController extends Controller
     {
         return "Hello, World!";
     }
+
+    public function idontwantthismethod()
+    {
+        return "This method should not be here.";
+    }
 }
